d38933850e3940cae5e6ebce637b92b0beaa505b
Yuchan Jung <67464014+YuchanJung@users.noreply.github.com>
2025-06-14 12:09:13 +0900

    Update client.py

--- client.py
+++ client.py
@@ -1,9 +1,8 @@
 import requests
 
-def check_samdasu(image_path):
+def predict(image_path):
     with open(image_path, "rb") as f:
         res = requests.post("http://localhost:8000/predict", files={"image": f})
     print("Result: ", res.json()["result"])
 
-# 사용
-check_samdasu("test.png")
+predict("test.png")
